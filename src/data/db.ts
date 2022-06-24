ee519bff2319580596dd68f9c0141d2fab747252
Barbara Kogus <barbarakogus@gmail.com>
2022-06-24 11:39:11 +0200

    add db prod

--- src/data/db.ts
+++ src/data/db.ts
@@ -1,25 +1,38 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
-    host: 'localhost',
-    database: 'postgres',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 3000,
-    connectionTimeoutMillis: 2000,
-    user: 'postgres',
-    password: 'postgres'
-});
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/postgres';
 
-const connectToDB = async () => {
-    try {
-        await pool.connect();
-    } catch (err) {
-        console.log(err)
+// const pool = new Pool({
+//     host: 'localhost',
+//     database: 'postgres',
+//     port: 5432,
+//     max: 10,
+//     idleTimeoutMillis: 3000,
+//     connectionTimeoutMillis: 2000,
+//     user: 'postgres',
+//     password: 'postgres'
+// });
+
+// const connectToDB = async () => {
+//     //     try {
+//     //         await pool.connect();
+//     //     } catch (err) {
+//     //         console.log(err)
+//     //     }
+//     // }
+
+//     // connectToDB();
+
+const poolConnection = process.env.DATABASE_URL ? {
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false
     }
-}
+} : {
+    connectionString
+};
 
-connectToDB(); 
+const pool = new Pool(poolConnection);
 
 export default {
     query: async (text: any, params?: any) => await pool.query(text, params),
@@ -27,8 +40,8 @@ export default {
     GET_TODO_LIST_BY_ID: 'SELECT id, name FROM "todo_list" WHERE id = $1',
     POST_TODO_LIST: 'INSERT INTO "todo_list" ("name") VALUES ($1) RETURNING id',
     DELETE_TODO_LIST: 'DELETE FROM "todo_list" WHERE id = $1',
-    GET_ALL_TODO_BY_TODO_LIST_ID: 'SELECT id, todo_list_id AS "todoListId", name, description, is_done AS "isDone" FROM "todo" WHERE todo_list_id = $1 ORDER BY is_done, id', 
-    GET_ALL_TODO_BY_ISDONE: 'SELECT id, todo_list_id AS "todoListId", name, description, is_done AS "isDone" FROM "todo" WHERE todo_list_id = $1 AND is_done = $2', 
+    GET_ALL_TODO_BY_TODO_LIST_ID: 'SELECT id, todo_list_id AS "todoListId", name, description, is_done AS "isDone" FROM "todo" WHERE todo_list_id = $1 ORDER BY is_done, id',
+    GET_ALL_TODO_BY_ISDONE: 'SELECT id, todo_list_id AS "todoListId", name, description, is_done AS "isDone" FROM "todo" WHERE todo_list_id = $1 AND is_done = $2',
     POST_TODO: 'INSERT INTO "todo" ("todo_list_id", name, description, is_done) VALUES ($1, $2, $3, $4) RETURNING id',
     DELETE_TODO: 'DELETE FROM "todo" WHERE id = $1',
     UPDATE_TODO: 'UPDATE "todo" SET is_done = NOT is_done where id = $1'
